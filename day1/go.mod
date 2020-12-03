fcd149eb4c4a8e9e30f3ffbddfbea2115c4dbc87
Nicholas Chang <nicholas.chang@alteryx.com>
2020-12-03 12:03:11 -0800

    init

---
+++ day1/go.mod
@@ -0,0 +1,3 @@
+module github.com/nick567187/advent-of-code-2020/day1
+
+go 1.15
---
+++ day1/test_sum2020.go
@@ -0,0 +1,13 @@
+package day1_test
+
+import (
+	"testing"
+
+	"github.com/nick567187/advent-of-code-2020/day1"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWorks(t *testing.T) {
+	day1.Find()
+	require.Equal(t, 0,0)
+}
