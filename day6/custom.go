5afca360b56225dae161790744829470a4dcf38a
Nicholas Chang <nicholas.chang@alteryx.com>
2020-12-15 17:08:07 -0800

    day 6

---
+++ day6/custom.go
@@ -0,0 +1,39 @@
+package day4
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func ReadCustoms() [][]string {
+	file, err := os.Open("custom.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var groups [][]string
+	var group []string
+	var i int
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text != "" {
+			group = append(group, text)
+		} else if text == "" {
+			groups[i] = group
+			group = []string{}
+		}
+	}
+	return groups
+}
+
+func CountCustoms(customs [][]string) (count int) {
+	for _, v := range customs {
+		for range v {
+			count++
+		}
+	}
+	return
+}
---
+++ day6/custom_test.go
@@ -0,0 +1,14 @@
+package day4_test
+
+import (
+	"testing"
+
+	"github.com/nick567187/advent-of-code-2020/day6"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCountCustoms(t *testing.T) {
+	i := [][]string{{"abc", "a"}}
+	r := day6.CountCustoms(i)
+	require.Equal(t, 4, r)
+}
