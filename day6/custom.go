4704497c351a329305a8a23d9d0f26de69567fc9
Nicholas Chang <nicholas.chang@alteryx.com>
2020-12-17 14:32:58 -0800

    finished day 6

--- day6/custom.go
+++ day6/custom.go
@@ -6,7 +6,10 @@ import (
 	"os"
 )
 
-func ReadCustoms() [][]string {
+type Groups [][]string
+type Group []string
+
+func ReadCustoms() Groups {
 	file, err := os.Open("custom.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -14,28 +17,52 @@ func ReadCustoms() [][]string {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var groups [][]string
-	var group []string
-	var i int
+	var groups Groups
+	var group Group
 	for scanner.Scan() {
 		text := scanner.Text()
 		if text != "" {
 			group = append(group, text)
 		} else if text == "" {
-			groups[i] = group
-			group = []string{}
+			groups = append(groups, group)
+			group = Group{}
 		}
+	}
+	if len(group) > 0 {
+		groups = append(groups, group)
 	}
 	return groups
 }
 
-func CountCustoms(customs [][]string) (count int) {
-	for _, custom := range customs {
-		for _, string := range custom {
-			for range string {
-				count++
+func CountQuestionsAnyoneYes(groups Groups) (count int) {
+	for _, group := range groups {
+		var letters [26]bool
+		for _, s := range group {
+			for _, v := range s {
+				var letterIndex = v - 97
+				if !letters[letterIndex] {
+					count++
+				}
+				letters[letterIndex] = true
 			}
  		}
+	}
+	return
+}
+
+func CountQuestionsEveryoneYes(groups Groups) (count int) {
+	for _, group := range groups {
+		var letters [26]int
+		var totalInGroup = len(group)
+		for _, s := range group {
+			for _, v := range s {
+				var letterIndex = v - 97
+				letters[letterIndex]++
+				if letters[letterIndex] == totalInGroup {
+					count++
+				}
+			}
+		}
 	}
 	return
 }
--- day6/custom_test.go
+++ day6/custom_test.go
@@ -7,8 +7,26 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCountCustoms(t *testing.T) {
-	i := [][]string{{"abc", "a"}}
-	r := day6.CountCustoms(i)
-	require.Equal(t, 4, r)
+func TestCountQuestionsAnyoneYes(t *testing.T) {
+	i := [][]string{{"abc"}, {"a", "b", "c"}, {"ab", "ac"}, {"a", "a", "a", "a"}, {"b"}}
+	r := day6.CountQuestionsAnyoneYes(i)
+	require.Equal(t, 11, r)
+}
+
+func TestCountQuestionsAnyoneOriginalInput(t *testing.T) {
+	i := day6.ReadCustoms()
+	r := day6.CountQuestionsAnyoneYes(i)
+	require.Equal(t, 6590, r)
+}
+
+func TestCountQuestionsEveryoneYes(t *testing.T) {
+	i := [][]string{{"abc"}, {"a", "b", "c"}, {"ab", "ac"}, {"a", "a", "a", "a"}, {"b"}}
+	r := day6.CountQuestionsEveryoneYes(i)
+	require.Equal(t, 6, r)
+}
+
+func TestCountQuestionsEveryoneOriginalInput(t *testing.T) {
+	i := day6.ReadCustoms()
+	r := day6.CountQuestionsEveryoneYes(i)
+	require.Equal(t, 3288, r)
 }
